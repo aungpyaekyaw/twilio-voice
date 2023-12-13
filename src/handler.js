a2be7bfb550ca833800e0068c5a71198d177a642
Aung Pyae <aungpyaekyaw@outlook.com>
2023-12-13 14:28:44 +0630

    feat: add conference page.

--- src/handler.js
+++ src/handler.js
@@ -1,19 +1,20 @@
-const VoiceResponse = require("twilio").twiml.VoiceResponse;
-const AccessToken = require("twilio").jwt.AccessToken;
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const AccessToken = require('twilio').jwt.AccessToken;
 const VoiceGrant = AccessToken.VoiceGrant;
 
-const nameGenerator = require("../name_generator");
-const config = require("../config");
+const nameGenerator = require('../name_generator');
+const config = require('../config');
+const client = require('twilio')(config.accountSid, config.authToken);
 
-var identity;
+let identity;
 
-exports.tokenGenerator = function tokenGenerator() {
-  identity = nameGenerator();
+exports.tokenGenerator = function tokenGenerator(mod) {
+  identity = mod ? 'Moderator' : nameGenerator();
 
   const accessToken = new AccessToken(
-    config.accountSid,
-    config.apiKey,
-    config.apiSecret
+      config.accountSid,
+      config.apiKey,
+      config.apiSecret
   );
   accessToken.identity = identity;
   const grant = new VoiceGrant({
@@ -32,30 +33,29 @@ exports.tokenGenerator = function tokenGenerator() {
 exports.voiceResponse = function voiceResponse(requestBody) {
   const toNumberOrClientName = requestBody.To;
   const callerId = config.callerId;
-  let twiml = new VoiceResponse();
+  const twiml = new VoiceResponse();
 
-  // If the request to the /voice endpoint is TO your Twilio Number, 
+  // If the request to the /voice endpoint is TO your Twilio Number,
   // then it is an incoming call towards your Twilio.Device.
   if (toNumberOrClientName == callerId) {
-    let dial = twiml.dial();
+    const dial = twiml.dial();
 
-    // This will connect the caller with your Twilio.Device/client 
+    // This will connect the caller with your Twilio.Device/client
     dial.client(identity);
-
   } else if (requestBody.To) {
     // This is an outgoing call
 
     // set the callerId
-    let dial = twiml.dial({ callerId });
+    const dial = twiml.dial({callerId});
 
     // Check if the 'To' parameter is a Phone Number or Client Name
-    // in order to use the appropriate TwiML noun 
-    const attr = isAValidPhoneNumber(toNumberOrClientName)
-      ? "number"
-      : "client";
+    // in order to use the appropriate TwiML noun
+    const attr = isAValidPhoneNumber(toNumberOrClientName) ?
+      'number' :
+      'client';
     dial[attr]({}, toNumberOrClientName);
   } else {
-    twiml.say("Thanks for calling!");
+    twiml.say('Thanks for calling!');
   }
 
   return twiml.toString();
@@ -68,4 +68,41 @@ exports.voiceResponse = function voiceResponse(requestBody) {
  */
 function isAValidPhoneNumber(number) {
   return /^[\d\+\-\(\) ]+$/.test(number);
+}
+
+exports.conferenceResponse = function conferenceResponse(requestBody) {
+  const twiml = new VoiceResponse();
+
+  const dial = twiml.dial();
+  const MODERATOR = 'Moderator';
+
+  if (requestBody.From == MODERATOR) {
+    dial.conference('My conference', {
+      startConferenceOnEnter: true,
+      endConferenceOnExit: true,
+    });
+  } else {
+    addUserToConference(requestBody.From, 'My conference', requestBody.From);
+  }
+
+  return twiml.toString();
+};
+
+
+/**
+ * Adds a user to a conference.
+ *
+ * @param {string} contact - The contact to add to the conference.
+ * @param {string} conferenceName - The name of the conference.
+ * @param {string} label - The label for the participant.
+ */
+function addUserToConference(contact, conferenceName, label) {
+  console.log(`adding user to conference: ${contact}`);
+  client.conferences(conferenceName)
+      .participants.create({
+        label: label,
+        from: '_from',
+        to: contact,
+        startConferenceOnEnter: true,
+      });
 }
--- src/router.js
+++ src/router.js
@@ -1,19 +1,25 @@
-const Router = require("express").Router;
-const { tokenGenerator, voiceResponse } = require("./handler");
+const Router = require('express').Router;
+const {tokenGenerator, voiceResponse,
+  conferenceResponse} = require('./handler');
 
 const router = new Router();
 
-router.get("/token", (req, res) => {
+router.get('/token', (req, res) => {
   res.send(tokenGenerator());
 });
 
-router.get("/token/moderator", (req, res) => {
+router.get('/token/mod', (req, res) => {
   res.send(tokenGenerator(true));
 });
 
-router.post("/voice", (req, res) => {
-  res.set("Content-Type", "text/xml");
+router.post('/voice', (req, res) => {
+  res.set('Content-Type', 'text/xml');
   res.send(voiceResponse(req.body));
+});
+
+router.post('/conference', (req, res) => {
+  res.set('Content-Type', 'text/xml');
+  res.send(conferenceResponse(req.body));
 });
 
 module.exports = router;
